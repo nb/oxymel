8c3524cb82f87b4f8a7369f4e0cf1fd3e139a25d
Nikolay Bachiyski <nb@nikolay.bg>
2013-02-02 11:59:14 -0500

    Move the tests into their own file

---
+++ TestOxymel.php
@@ -0,0 +1,133 @@
+<?php
+require_once __DIR__ . '/Oxymel.php';
+class TestX extends PHPUnit_Framework_TestCase {
+	function __construct() {
+		$this->x = new Oxymel;
+	}
+	function test_self_closing() {
+		$this->a('<baba/>', $this->x->baba);
+	}
+	function test_self_closing_method() {
+		$this->a('<baba/>', $this->x->baba());
+	}
+	function test_attribute_no_content() {
+		$this->a('<baba a="b" c="d"/>', $this->x->baba( array( 'a' => 'b', 'c' => 'd' ) ) );
+	}
+
+	function test_content_no_attribute() {
+		$this->a('<baba>content</baba>', $this->x->baba( 'content' ) );
+	}
+
+	function test_escapiing_of_content() {
+		$this->a('<baba>&lt;</baba>', $this->x->baba( '<' ) );
+	}
+
+	function test_escaping_of_attributes() {
+		$this->a('<baba a="&lt;"/>', $this->x->baba( array( 'a' => '<' ) ) );
+	}
+
+	function test_content_and_attributes() {
+		$this->a('<baba a="b" c="d">content</baba>', $this->x->baba( 'content', array( 'a' => 'b', 'c' => 'd' ) ) );
+	}
+
+	function x_test_content_and_attributes_error() {
+		$this->a('<baba a="b" c="d">content</baba>', $this->x->baba( array( 'a' => 'b', 'c' => 'd' ), 'content' ) );
+	}
+
+	function test_go_down() {
+		$this->a('<baba>
+  <dyado/>
+</baba>', $this->x->baba->contains->dyado );
+	}
+
+	function test_go_down_and_up() {
+		$this->a("<level0>
+  <level1/>
+</level0>
+<level0/>", $this->x->level0->contains->level1->end->level0 );
+	}
+
+	function test_cdata() {
+		$this->a('<baba><![CDATA[content]]></baba>', $this->x->baba->contains->cdata('content')->end);
+	}
+
+	function test_raw() {
+		$this->a('<baba>
+  <dyado/>
+</baba>', $this->x->baba->contains->raw('<dyado></dyado>')->end);
+	}
+
+	function x_test_only_up_error() {
+		$this->a('', $this->x->end );
+	}
+
+	function test_open_in_the_end() {
+		$this->a("<baba/>
+<newtag>", $this->x->baba->open_newtag );
+	}
+
+	function test_open_with_attributes() {
+		$this->a("<baba/>
+<newtag a=\"b\">", $this->x->baba->open_newtag( array( 'a' => 'b' ) ) );
+	}
+
+	function test_dom_open_dom() {
+		$this->a("<baba/>
+<newtag>
+<baba/>", $this->x->baba->open_newtag->baba );
+	}
+
+	function test_close_in_the_beginning() {
+		$this->a("</oldtag>
+<baba/>", $this->x->close_oldtag->baba );
+	}
+
+	function test_dom_close_dom() {
+		$this->a("<baba/>
+</oldtag>
+<baba/>", $this->x->baba->close_oldtag->baba );
+	}
+
+	function test_baba() {
+		$this->a("<a>baba<x/></a>", $this->x->a('baba')->contains->x );
+	}
+
+	function test_comment() {
+		$this->a('<!--baba-->', $this->x->comment('baba'));
+	}
+
+	function test_two_invocations() {
+		$this->x->baba();
+		$this->x->dyado();
+		$this->a( "<baba/>
+<dyado/>", $this->x );
+	}
+
+	function test_double_end() {
+		$this->a( "<out>
+  <mid>
+    <in/>
+  </mid>
+</out>
+<nextout/>", $this->x->out->contains->mid->contains->in->end->end->nextout );
+	}
+
+	function test_long() {
+		echo $this->x->xml->html->contains
+  ->head->contains
+    ->meta(array('charset' => 'utf-8'))
+    ->title("How to seduce dragons")
+    ->end
+  ->body(array('class' => 'story'))->contains
+    ->h1('How to seduce dragons')
+    ->h2('The fire manual')
+    ->p('Once upon a time in a distant land there was an dragon.')
+    ->p('In another very distant land')->contains
+		->text(' there was a very ')->strong('strong')->text(' warrrior')->end
+	->p->contains->cdata('<b>sadad</b>');
+	}
+
+	private function a($value, $x) {
+		$this->assertEquals( $value . "\n", (string)$x);
+	}
+}
